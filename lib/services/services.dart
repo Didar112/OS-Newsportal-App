eb06ae9b2c394b982ef91d37d1368d9988398e20
didar <didaruli36@gmail.com>
2024-12-11 21:25:18 +0600

    initial commit

---
+++ lib/services/services.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:app_5/model/new_model.dart';
+
+
+class NewsApi {
+
+  List<NewsModel> dataStore = [];
+  Future<void> getNews() async {
+    Uri url = Uri.parse(
+        "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=47d5376ba95b47a49ad53edb38eeacda");
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+    if (jsonData["status"] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null &&
+            element['description'] != null &&
+            element['author'] != null &&
+            element['content'] != null) {
+          NewsModel newsModel = NewsModel(
+            title: element['title'],
+            urlToImage: element['urlToImage'],
+            description: element['description'],
+            author: element['author'],
+            content: element['content'],
+          );
+          dataStore.add(newsModel);
+        }
+      });
+    }
+  }
+}
+
+class CategoryNews {
+
+  List<NewsModel> dataStore = [];
+  Future<void> getNews(String category) async {
+    Uri url = Uri.parse(
+        "https://newsapi.org/v2/top-headlines?country=us&category=$category&apiKey=47d5376ba95b47a49ad53edb38eeacda");
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData["status"] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null &&
+            element['description'] != null &&
+            element['author'] != null &&
+            element['content'] != null) {
+          NewsModel newsModel = NewsModel(
+            title: element['title'],
+            urlToImage: element['urlToImage'],
+            description: element['description'],
+            author: element['author'],
+            content: element['content'],
+          );
+          dataStore.add(newsModel);
+        }
+      });
+    }
+  }
+}
